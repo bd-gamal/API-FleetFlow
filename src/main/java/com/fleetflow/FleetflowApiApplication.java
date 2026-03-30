db6102147ece4f93763799694b2f2eb25c35aeef
Gamal Badie <149597937+bd-gamal@users.noreply.github.com>
2026-03-30 11:18:35 +0100

    first commit

---
+++ src/main/java/com/fleetflow/FleetflowApiApplication.java
@@ -0,0 +1,13 @@
+package com.fleetflow;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class FleetflowApiApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(FleetflowApiApplication.class, args);
+	}
+
+}
