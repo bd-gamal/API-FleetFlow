172232712876716294452c971cbb41fbb201e420
Samira12-dev <samiraelboussidi663@gmail.com>
2026-04-01 17:39:35 +0100

    Fixing some errors

--- src/main/java/com/fleetflow/dto/Livraisondto.java
+++ src/main/java/com/fleetflow/dto/Livraisondto.java
@@ -2,14 +2,20 @@ package com.fleetflow.dto;
 
 import com.fleetflow.entity.Client;
 import com.fleetflow.entity.StatusLivraison;
+import lombok.AllArgsConstructor;
+import lombok.Data;
+import lombok.NoArgsConstructor;
 
 import java.time.LocalDate;
 
+@Data
+@AllArgsConstructor
+@NoArgsConstructor
 public class Livraisondto {
     private LocalDate dateLivraison;
     private String adresseDepart;
     private  String adresseDestination;
-    private StatusLivraison status;
+    private StatusLivraison statusLivraison;
     private Long clientId;
 
 }
