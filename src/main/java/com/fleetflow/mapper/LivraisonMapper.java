f0c0556b20feb6109cfeb12a446b402dffecd047
Samira12-dev <samiraelboussidi663@gmail.com>
2026-04-02 15:04:52 +0100

    add swagger dependency

--- src/main/java/com/fleetflow/mapper/LivraisonMapper.java
+++ src/main/java/com/fleetflow/mapper/LivraisonMapper.java
@@ -11,8 +11,8 @@ import java.util.List;
 @Mapper(componentModel = "spring")
 public interface LivraisonMapper {
     @Mapping(source = "client.id", target = "clientId")
-    @Mapping(source = "vehicule.id", target = "vehiculeId")
-    @Mapping(source = "chauffeur.id", target = "chauffeurId")
+//    @Mapping(source = "vehicule.id", target = "vehiculeId")
+//    @Mapping(source = "chauffeur.id", target = "chauffeurId")
     LivraisonRequestDTO toDto(Livraison livraison);
 
     @Mapping(source ="clientId", target ="client.id")
